5f439ec66c5dee08fe98d753c4b9e0d8e86e3a0d
sherazahra <sherazahra30@gmail.com>
2024-03-07 09:58:14 +0700

    modul 8

--- expresbasic/routes/kategori.js
+++ expresbasic/routes/kategori.js
@@ -1,105 +1,70 @@
 var express = require("express");
-var connection = require("../config/database");
+var router = express.Router();
+var connection = require("../config/database.js");
+const Model_Kategori = require("../model/Model_Kategori");
 
-let router = express.Router();
-
-router.get("/", (req, res, next) => {
-  connection.query(
-    "SELECT * FROM kategori ORDER BY id_kategori DESC",
-    (err, rows) => {
-      if (err) {
-        req.flash("error", err);
-      } else {
-        res.render("kategori/index", {
-          data: rows,
-        });
-      }
-    }
-  );
+router.get("/", async function (req, res, next) {
+  let rows = await Model_Kategori.getAll();
+  res.render("kategori/index", {
+    data: rows,
+  });
 });
 
-router.get("/create", (req, res, next) => {
-  res.render("kategori/create", { nama_kategori: "" });
+router.get("/create", function (req, res, next) {
+  res.render("kategori/create", {
+    nama_kategori: " ",
+  });
 });
 
-router.post("/store", (req, res, next) => {
+router.post("/store", async function (req, res, next) {
   try {
     let { nama_kategori } = req.body;
-    let data = {
-      nama_kategori,
-    };
-    connection.query("INSERT INTO kategori set ? ", data, (err, result) => {
-      if (err) {
-        req.flash("error", "Gagal menyimpan data");
-      } else {
-        req.flash("success", "Berhasil menyimpan data");
-      }
-      res.redirect("/kategori");
-    });
+    let Data = { nama_kategori }; // Assuming you want to store the 'nama_kategori' in an object
+
+    await Model_Kategori.Store(Data);
+
+    req.flash("success", "Berhasil menyimpan data");
+    res.redirect("/kategori");
   } catch (error) {
-    req.flash("error", "Terjadi kesalahan");
+    req.flash("error", "Gagal menyimpan data");
     res.redirect("/kategori");
   }
 });
 
-router.get("/edit/(:id)", (req, res, next) => {
-  let { id } = req.params;
+router.get("/update/(:id)", async function (req, res, next) {
+  let id = req.params.id;
+  let rows = await Model_Kategori.getId(id);
 
-  connection.query(
-    "SELECT * FROM kategori WHERE id_kategori=?",
-    id,
-    (err, rows) => {
-      if (err) {
-        req.flash("error", err);
-      } else {
-        res.render("kategori/update", {
-          data: rows[0],
-        });
-      }
-    }
-  );
+  res.render("kategori/update", {
+    id: rows[0].id_kategori,
+    nama_kategori: rows[0].nama_kategori,
+  });
 });
 
-router.post("/update/(:id)", (req, res, next) => {
+router.post("/update/:id", async function (req, res, next) {
   try {
-    let { id } = req.params;
+    let id = req.params.id;
     let { nama_kategori } = req.body;
-    let data = {
-      nama_kategori,
-    };
-    connection.query(
-      "UPDATE kategori set ? where id_kategori = " + id,
-      data,
-      (err, result) => {
-        if (err) {
-          req.flash("error", "Gagal mengupdate data");
-        } else {
-          req.flash("success", "Berhasil mengupdate data");
-        }
-        res.redirect("/kategori");
-      }
-    );
+    let Data = { nama_kategori }; // Assuming you want to update the 'nama_kategori' field
+
+    await Model_Kategori.Update(id, Data);
+
+    req.flash("success", "Berhasil menyimpan data");
+    res.redirect("/kategori");
   } catch (error) {
-    req.flash("error", "Terjadi kesalahan");
+    // Handle error, you can redirect or render an error page
+    console.error(error);
+    req.flash("error", "Gagal menyimpan data");
     res.redirect("/kategori");
   }
 });
 
-router.get("/delete/(:id)", (req, res, next) => {
-  let { id } = req.params;
+router.get("/delete/(:id)", async function (req, res) {
+  let id = req.params.id;
+  await Model_Kategori.Delete(id);
 
-  connection.query(
-    "DELETE FROM kategori WHERE id_kategori=?",
-    id,
-    (err, rows) => {
-      if (err) {
-        req.flash("error", "Gagal Menghapus Data");
-      } else {
-        req.flash("success", "Berhasil Menghapus Data");
-      }
-      res.redirect("/kategori");
-    }
-  );
+  req.flash("success", "Berhasil menghapus data");
+  res.redirect("/kategori");
 });
 
 module.exports = router;
